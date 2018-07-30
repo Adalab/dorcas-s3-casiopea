e174f56261723dff3269c3a2be100d09e718a457
Sofía Rodríguez <sofiarincon.artandmath@gmail.com>
2018-07-30 12:25:48 +0200

    función

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ class App extends Component {
   escribe() {
     alert("Muy bien escrito");
   }
+  escatologia(){
+    alert("culo");
+  }
 
   render() {
     return (
@@ -42,6 +45,7 @@ class App extends Component {
           BotonPuton!
         </button>
         <input onKeyUp={this.escribe} type="text"/>
+        <input onKeyUp={this.escatologia} type="text" />
         <Pag
           copyright={this.state.footer.texto}
           image={this.state.footer.image}
