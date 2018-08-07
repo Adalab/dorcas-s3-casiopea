13d1fc8e5de2caa8672f95e9a842c0b36c7bb2ad
Aylecilla <aylenortiz@gmail.com>
2018-08-07 11:57:03 +0200

    recolviendo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 import logoAda from "../images/logo-adalab-80px.png";
 
 class Footer extends React.Component {
-  render(){
+  render() {
     return (
       <div className="container__footer">
         <footer className="footer">
           <p className="footer__copyright">{this.props.copy}</p>
           <a href={this.props.href} target="_Blank">
-          <img
-            className="footer__logo--adalab"
-            src={this.props.img}
-            alt="Logotipo de Adalab"
-          />
+            <img
+              className="footer__logo--adalab"
+              src={this.props.img}
+              alt="Logotipo de Adalab"
+            />
           </a>
         </footer>
       </div>
