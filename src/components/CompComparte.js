c97119d0cc298378dbec9882cb3e448d4e76c4e9
Veronica Bautista <veronicabautista87@gmail.com>
2018-08-09 18:52:32 +0200

    botones compartir

--- src/components/CompComparte.js
+++ src/components/CompComparte.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class CompComparte extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleCreateButton = this.handleCreateButton.bind(this);
+    this.handleShareButton = this.handleShareButton.bind(this);
+  }
+
+  handleCreateButton() {
+    console.log('tarjeta creada');
+  }
+
+  handleShareButton() {
+    console.log('comparte tarjeta');
+  }
+
+
   render(){
     return (
       <fieldset className="div__rellena collapsible fieldset__rellena fieldset--invisible">
@@ -19,6 +35,7 @@ class CompComparte extends React.Component {
               id="submit"
               type="button"
               className="fieldset__share--button"
+              onClick={this.handleCreateButton} 
               >
                 <span className="fieldset__share--button--icon">
                   <i className="far fa-address-card" />
@@ -31,7 +48,8 @@ class CompComparte extends React.Component {
             <div class="fieldset__share--created">
               <p>La tarjeta ha sido creada:</p>
               <p class="fieldset__share--link"></p>
-              <button type="button" name="button" class="fieldset__share--button-twitter">
+              <button type="button" name="button" class="fieldset__share--button-twitter"
+              onClick={this.handleShareButton} >
                 <i class="fab fa-twitter"></i> Compartir en Twitter
               </button>
             </div>
@@ -43,3 +61,34 @@ class CompComparte extends React.Component {
 }
 
 export default CompComparte;
+
+// // Crear enlace
+// sendRequest(userForm){
+//   fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+//     method: 'POST',
+//     body: JSON.stringify(userForm),
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//   })
+//     .then(function(resp) { return resp.json(); })
+//     .then(function(result) { showURL(result); })
+//     .catch(function(error) { console.log(error); });
+// }
+// showURL(result){
+//   if(result.success){
+//     responseURL.innerHTML = '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
+//   }else{
+//     responseURL.innerHTML = 'ERROR:' + result.error;
+//   }
+// }
+// //BOTON TWITTER
+// var twitterShare = document.querySelector('.container__comparte--button-twitter');
+
+// twitterShare.onclick = function(e) {
+//   var linkTwitter = document.querySelector('.container__comparte-link  a').href;
+//   e.preventDefault();
+//   var twitterWindow = window.open('https://twitter.com/share?url=' + linkTwitter, 'twitter-popup', 'height=350,width=600','590','253');
+//   if(twitterWindow.focus) { twitterWindow.focus(); }
+//   return false;
+// };
